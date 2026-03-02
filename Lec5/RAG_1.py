4a6f0812ba240577d53404fd9353910c0578ab10
nitinkumar <nitinkumar18ad@gmail.com>
2026-03-03 00:53:04 +0530

    asdf

--- Lec5/RAG_1.py
+++ Lec5/RAG_1.py
@@ -11,7 +11,7 @@ docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
-     chunk_overlap=200,
+     chunk_overlap=100,
 )
 
 split_docs = text_splitter.split_documents(documents=docs)
