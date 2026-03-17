3bc752696a34b8d6edc6a6fcd9c8f9dd61c3c3a8
nitinkumar <nitinkumar18ad@gmail.com>
2026-03-17 22:23:11 +0530

    17march

--- Lec3/weather_agent.py
+++ Lec3/weather_agent.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import os
 from langsmith.wrappers import wrap_openai 
+from langfuse import Langfuse
 
 
 # ------------------ INIT ------------------
