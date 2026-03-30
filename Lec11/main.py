117f5b70fa945f44684d668e9b90c6e0387a6a3c
nitinkumar <nitinkumar18ad@gmail.com>
2026-03-30 21:56:35 +0530

    30march

--- Lec11/main.py
+++ Lec11/main.py
@@ -10,7 +10,7 @@ def init():
     with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
         graph_with_mongo = create_chat_graph(checkpointer=checkpointer)
         while True:
-            user_input = input(">")
+            user_input = input(" >>> ")
             for event in graph_with_mongo.stream({"messages":[{"role":"user","content": user_input}]},config,stream_mode="values"):
                 if "messages" in event:
                     event["messages"][-1].pretty_print()
